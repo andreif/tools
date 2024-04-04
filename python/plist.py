f58af7d6078c48a62a67877eaf929ca650858fbb
Andrei Fokau <andrei@funnel.io>
2024-04-04 21:29:45 +0200

    aws cli bun

--- python/plist.py
+++ python/plist.py
@@ -6,20 +6,46 @@ import subprocess
 import sys
 
 
-def parse(path):
-    p = subprocess.run(['defaults', 'read', os.path.abspath(path)],
-                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def plist_to_json(text):
+    s = re.sub(r' = ([^"]+);\n', ' = "\\1",\n', text)
+    s = re.sub(r'(\S+) =\s*', '"\\1": ', s)
+    s = re.sub(r':\s+\(', ': [', s)
+    s = re.sub(r';\n', ',\n', s)
+    s = re.sub(r'\)(,?)\n', ']\\1\n', s)
+    s = re.sub(r',(\s+[}\]])', '\\1', s)
+    return s
+
+
+def defaults(*args):
+    p = subprocess.run(['defaults', *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if p.returncode:
         raise NotImplementedError(p.stderr.decode())
-    o = p.stdout.decode()
-    o = re.sub(r' = ([^"]+);\n', ' = "\\1",\n', o)
-    o = re.sub(r'(\S+) =\s*', '"\\1": ', o)
-    o = re.sub(r':\s+\(', ': [', o)
-    o = re.sub(r';\n', ',\n', o)
-    o = re.sub(r'\)(,?)\n', ']\\1\n', o)
-    o = re.sub(r',(\s+[}\]])', '\\1', o)
-    return json.loads(o)
+    return p.stdout.decode()
+
+
+def parse(text):
+    return json.loads(plist_to_json(text))
+
+
+def read(path):
+    return parse(text=defaults('read', path))
+
+
+def main(_, path=None):
+    if sys.stdin.isatty():
+        if path is None:
+            print('Error: Pipe or path to plist file required', file=sys.stderr)
+            exit(1)
+        print(json.dumps(read(path=os.path.abspath(path))))
+    else:
+        text = sys.stdin.read()
+        try:
+            data = json.loads(text)
+        except Exception:
+            print(text)
+        else:
+            print(data)
 
 
 if __name__ == '__main__':
-    print(json.dumps(parse(sys.argv[1])))
+    main(*sys.argv)
